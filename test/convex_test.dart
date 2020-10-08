a2f1e649368b0a3ece33c52358cdc6b22512c7ae
Dee Pheap <pheapdee35@gmail.com>
2020-10-08 12:34:18 +0700

    Test Convex Scrypt

--- test/convex_test.dart
+++ test/convex_test.dart
@@ -7,7 +7,7 @@ var _heroAddress =
     '7E66429CA9c10e68eFae2dCBF1804f0F6B3369c7164a3187D6233683c258710f';
 
 void main() {
-  group('Query', () {
+  group('Query - Convex Lisp', () {
     test('Inc', () async {
       var response = await convex.query(
           scheme: 'http',
@@ -26,6 +26,34 @@ void main() {
           host: '127.0.0.1',
           port: 8080,
           source: '*address*',
+          address: _heroAddress);
+
+      expect(response.statusCode, 200);
+      expect(convert.jsonDecode(response.body), {'value': '0x' + _heroAddress});
+    });
+  });
+
+  group('Query - Convex Scrypt', () {
+    test('Inc', () async {
+      var response = await convex.query(
+          scheme: 'http',
+          host: '127.0.0.1',
+          port: 8080,
+          source: 'inc(1)',
+          lang: convex.Lang.convexScript,
+          address: _heroAddress);
+
+      expect(response.statusCode, 200);
+      expect(convert.jsonDecode(response.body), {'value': 2});
+    });
+
+    test('Self Address', () async {
+      var response = await convex.query(
+          scheme: 'http',
+          host: '127.0.0.1',
+          port: 8080,
+          source: '_address_',
+          lang: convex.Lang.convexScript,
           address: _heroAddress);
 
       expect(response.statusCode, 200);
