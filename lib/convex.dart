e04058eaf66531435b6e6f69f1195d9de0ffb0f8
Dee Pheap <pheapdee35@gmail.com>
2020-10-09 14:12:12 +0700

    Transact

--- lib/convex.dart
+++ lib/convex.dart
@@ -54,6 +54,138 @@ Future<http.Response> query({
   return client.post(uri, body: body);
 }
 
+Future<http.Response> prepareTransaction({
+  http.Client client,
+  String scheme = 'https',
+  String host = CONVEX_WORLD_HOST,
+  int port = 443,
+  String source,
+  String address,
+  int sequenceNumber,
+  Lang lang = Lang.convexLisp,
+}) {
+  var uri = Uri(
+      scheme: scheme,
+      host: host,
+      port: port,
+      path: 'api/v1/transaction/prepare');
+
+  Map<String, dynamic> body = {
+    'source': source,
+    'address': address,
+    'lang': langString(lang),
+  };
+
+  if (sequenceNumber != null) {
+    body['sequenceNumber'] = sequenceNumber;
+  }
+
+  var bodyEncoded = convert.jsonEncode(body);
+
+  if (client == null) {
+    return http.post(uri, body: bodyEncoded);
+  }
+
+  return client.post(uri, body: bodyEncoded);
+}
+
+Future<http.Response> submitTransaction({
+  http.Client client,
+  String scheme = 'https',
+  String host = CONVEX_WORLD_HOST,
+  int port = 443,
+  String address,
+  String hash,
+  String sig,
+}) {
+  var uri = Uri(
+      scheme: scheme,
+      host: host,
+      port: port,
+      path: 'api/v1/transaction/submit');
+
+  Map<String, dynamic> body = {
+    'hash': hash,
+    'address': address,
+    'sig': sig,
+  };
+
+  var bodyEncoded = convert.jsonEncode(body);
+
+  if (client == null) {
+    return http.post(uri, body: bodyEncoded);
+  }
+
+  return client.post(uri, body: bodyEncoded);
+}
+
+class Result {
+  Result({
+    this.id,
+    this.value,
+    this.errorCode,
+  });
+
+  int id;
+  dynamic value;
+  String errorCode;
+}
+
+Future<Result> transact({
+  http.Client client,
+  String scheme = 'https',
+  String host = CONVEX_WORLD_HOST,
+  int port = 443,
+  String source,
+  String address,
+  int sequenceNumber,
+  Lang lang = Lang.convexLisp,
+  Uint8List secretKey,
+}) async {
+  var prepareResponse = await prepareTransaction(
+    client: client,
+    scheme: scheme,
+    host: host,
+    port: port,
+    source: source,
+    address: address,
+    sequenceNumber: sequenceNumber,
+    lang: lang,
+  );
+
+  var prepareBody = convert.jsonDecode(prepareResponse.body);
+
+  var hashHex = prepareBody['hash'];
+  var hashBin = sodium.Sodium.hex2bin(hashHex);
+
+  var sigBin = sign(hashBin, secretKey);
+  var sigHex = sodium.Sodium.bin2hex(sigBin);
+
+  print('Transact - Hash $hashHex, Sig $sigHex');
+
+  var submitResponse = await submitTransaction(
+    client: client,
+    scheme: scheme,
+    host: host,
+    port: port,
+    address: address,
+    hash: hashHex,
+    sig: sigHex,
+  );
+
+  var submitBody = convert.jsonDecode(submitResponse.body);
+
+  if (submitResponse.statusCode != 200) {
+    throw Exception(submitBody['error']['message']);
+  }
+
+  return Result(
+    id: submitBody['id'],
+    value: submitBody['value'],
+    errorCode: submitBody['error-code'],
+  );
+}
+
 Future<http.Response> account({
   http.Client client,
   String scheme = 'https',
