1d9cd760ffbcdb9b1ed4465c0f1055c393c3b952
Dee Pheap <pheapdee35@gmail.com>
2020-11-04 10:42:54 +0700

    Launcher screen

--- lib/route.dart
+++ lib/route.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
 
+import 'screen/launcher.dart';
 import 'screen/home.dart';
 import 'screen/wallet.dart';
 import 'screen/account.dart';
 
-const String home = '/';
+const String launcher = '/';
+const String home = '/home';
 const String wallet = '/wallet';
 const String account = '/account';
 
 Map<String, WidgetBuilder> routes() => {
+      launcher: (context) => LauncherScreen(),
       home: (context) => HomeScreen(),
       wallet: (context) => WalletScreen(),
       account: (context) => AccountScreen(),
---
+++ lib/screen/launcher.dart
@@ -0,0 +1,43 @@
+import 'package:convex_wallet/model.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../widget.dart';
+import './home.dart';
+
+class LauncherScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<AppState>();
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Convexity'),
+        actions: [
+          if (appState.model.allKeyPairs.isNotEmpty)
+            IdenticonDropdown(
+              activeKeyPair: appState.model.activeKeyPairOrDefault(),
+              allKeyPairs: appState.model.allKeyPairs,
+            ),
+        ],
+      ),
+      body: HomeScreenBody(),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_balance_wallet),
+            label: 'Wallet',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_circle),
+            label: 'Account',
+          ),
+        ],
+      ),
+    );
+  }
+}
