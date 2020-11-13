22d9d283a80d52634ffc7223933ff3accdeed396
Dee Pheap <pheapdee35@gmail.com>
2020-11-13 16:53:09 +0700

    Pick an option

--- lib/screen/new_asset.dart
+++ lib/screen/new_asset.dart
@@ -1,5 +1,13 @@
 import 'package:flutter/material.dart';
 
+enum _EntryType {
+  recommended,
+  scanQRCode,
+  search,
+  myHoldings,
+  assetId,
+}
+
 class NewAssetScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -16,10 +24,51 @@ class NewAssetScreenBody extends StatefulWidget {
 }
 
 class _NewAssetScreenBodyState extends State<NewAssetScreenBody> {
+  var selectedType = _EntryType.recommended;
+
+  Widget option({
+    @required String title,
+    @required _EntryType value,
+  }) =>
+      RadioListTile(
+        title: Text(title),
+        value: value,
+        groupValue: selectedType,
+        onChanged: (value) {
+          setState(() {
+            selectedType = value;
+          });
+        },
+      );
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [Text('Hello')],
+    return Padding(
+      padding: const EdgeInsets.all(12),
+      child: Column(
+        children: [
+          option(
+            title: 'Recommended',
+            value: _EntryType.recommended,
+          ),
+          option(
+            title: 'Scan QR Code',
+            value: _EntryType.scanQRCode,
+          ),
+          option(
+            title: 'Search',
+            value: _EntryType.search,
+          ),
+          option(
+            title: 'My Holdings',
+            value: _EntryType.myHoldings,
+          ),
+          option(
+            title: 'Entry Asset ID',
+            value: _EntryType.assetId,
+          ),
+        ],
+      ),
     );
   }
 }
