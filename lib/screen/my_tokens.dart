3613dd85bc888d3664d7f0468fe4616fff8c8bcc
Pedro Girardi <pedro@plando.work>
2020-11-30 12:50:00 +0700

    My Tokens - GridView example

--- lib/screen/my_tokens.dart
+++ lib/screen/my_tokens.dart
@@ -21,8 +21,44 @@ class MyTokensScreen extends StatelessWidget {
 class MyTokensScreenBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('My Tokens'),
+    return GridView.count(
+      primary: false,
+      padding: const EdgeInsets.all(20),
+      crossAxisSpacing: 10,
+      mainAxisSpacing: 10,
+      crossAxisCount: 2,
+      children: <Widget>[
+        Container(
+          padding: const EdgeInsets.all(8),
+          child: const Text("He'd have you all unravel at the"),
+          color: Colors.teal[100],
+        ),
+        Container(
+          padding: const EdgeInsets.all(8),
+          child: const Text('Heed not the rabble'),
+          color: Colors.teal[200],
+        ),
+        Container(
+          padding: const EdgeInsets.all(8),
+          child: const Text('Sound of screams but the'),
+          color: Colors.teal[300],
+        ),
+        Container(
+          padding: const EdgeInsets.all(8),
+          child: const Text('Who scream'),
+          color: Colors.teal[400],
+        ),
+        Container(
+          padding: const EdgeInsets.all(8),
+          child: const Text('Revolution is coming...'),
+          color: Colors.teal[500],
+        ),
+        Container(
+          padding: const EdgeInsets.all(8),
+          child: const Text('Revolution, they...'),
+          color: Colors.teal[600],
+        ),
+      ],
     );
   }
 }
