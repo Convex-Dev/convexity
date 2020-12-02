e7c3c97b9881af7c16590381c9a2e640927a24f1
Pedro Girardi <pedro@plando.work>
2020-12-02 16:54:59 +0700

    Fix Fungible balance formatting

--- lib/format.dart
+++ lib/format.dart
@@ -1,12 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 import 'convex.dart';
 
 String formatFungibleCurrency({
   @required FungibleTokenMetadata metadata,
   @required int number,
 }) =>
-    NumberFormat.simpleCurrency(
-      name: metadata.symbol,
-      decimalDigits: metadata.decimals,
-    ).format(number);
+    '${metadata.currencySymbol}${number.toStringAsFixed(metadata.decimals)}';
