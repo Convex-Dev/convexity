6b8682b09222bfce3a907e4b8ebe21313d3488d4
samdy <samdyrupp@gmail.com>
2020-12-22 13:56:43 +0700

    account screen update

--- lib/screen/account.dart
+++ lib/screen/account.dart
@@ -1,8 +1,12 @@
+import 'package:convex_wallet/widget.dart';
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
+import 'package:qr_flutter/qr_flutter.dart';
 
 import '../convex.dart';
+import '../logger.dart';
 import '../model.dart';
 
 class AccountScreen extends StatelessWidget {
@@ -39,98 +43,164 @@ class _AccountScreenBodyState extends State<AccountScreenBody> {
         .account(address: widget.address);
   }
 
-  @override
-  Widget build(BuildContext context) => FutureBuilder(
-        future: account,
-        builder: (BuildContext context, AsyncSnapshot<Account> snapshot) {
-          var progressIndicator = Center(child: CircularProgressIndicator());
-
-          switch (snapshot.connectionState) {
-            case ConnectionState.none:
-              return progressIndicator;
-            case ConnectionState.waiting:
-              return progressIndicator;
-            case ConnectionState.active:
-              return progressIndicator;
-            case ConnectionState.done:
-              var account = snapshot.data;
-
-              if (account == null) {
-                return Padding(
-                  padding: const EdgeInsets.all(8),
-                  child: Center(
-                    child: Text('Account not found.'),
-                  ),
-                );
-              }
-
-              final tiles = [
-                // -- Address
-                ListTile(
-                  title: SelectableText(
-                    account.address.toString(),
-                  ),
-                  subtitle: Text('Address'),
-                ),
-
-                // -- Type
-                ListTile(
-                  title: Text(account.type.toString()),
-                  subtitle: Text('Type'),
-                ),
-
-                // -- Balance
-                ListTile(
-                  title: Text(account.balance.toString()),
-                  subtitle: Text('Balance'),
-                ),
-
-                // -- Memory Size
-                ListTile(
-                  title: Text(account.memorySize.toString()),
-                  subtitle: Text('Memory Size'),
-                ),
-
-                // -- Memory Allowance
-                ListTile(
-                  title: Text(account.memoryAllowance.toString()),
-                  subtitle: Text('Memory Allowance'),
-                ),
-              ];
-
-              final animated = tiles
-                  .asMap()
-                  .entries
-                  .map(
-                    (e) => AnimationConfiguration.staggeredList(
-                      position: e.key,
-                      duration: const Duration(milliseconds: 375),
-                      child: SlideAnimation(
-                        verticalOffset: 50.0,
-                        child: FadeInAnimation(
-                          child: e.value,
-                        ),
-                      ),
+  Widget _addressInfo(String account) {
+    return StatelessWidgetBuilder(
+      (context) => Card(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Name',
+                      textAlign: TextAlign.center,
+                      style: Theme.of(context).textTheme.bodyText1,
                     ),
-                  )
-                  .toList();
+                    Gap(4),
+                    Text(
+                      'description',
+                      textAlign: TextAlign.center,
+                      style: Theme.of(context).textTheme.bodyText2,
+                    ),
+                    Gap(4),
+                    SelectableText(
+                      account,
+                      showCursor: false,
+                      style: Theme.of(context).textTheme.caption,
+                    ),
+                  ],
+                ),
+              ),
+              QrImage(
+                data: widget.address.hex,
+                version: QrVersions.auto,
+                size: 80,
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 
+  Widget _buildContent({@required String account, @required String label}) {
+    return StatelessWidgetBuilder(
+      (context) => Card(
+        margin: EdgeInsets.symmetric(vertical: 8.0),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5.0)),
+        child: Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: Column(
+            children: <Widget>[
+              Container(
+                margin: EdgeInsets.all(10),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      account,
+                      style: TextStyle(fontSize: 16.0),
+                    ),
+                    Text(
+                      label,
+                      style: TextStyle(fontSize: 16.0),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: account,
+      builder: (BuildContext context, AsyncSnapshot<Account> snapshot) {
+        var progressIndicator = Center(child: CircularProgressIndicator());
+
+        switch (snapshot.connectionState) {
+          case ConnectionState.none:
+            return progressIndicator;
+          case ConnectionState.waiting:
+            return progressIndicator;
+          case ConnectionState.active:
+            return progressIndicator;
+          case ConnectionState.done:
+            var account = snapshot.data;
+
+            if (account == null) {
               return Padding(
-                padding: const EdgeInsets.all(12),
-                child: AnimationLimiter(
-                  child: ListView(
-                    children: animated,
-                  ),
+                padding: const EdgeInsets.all(8),
+                child: Center(
+                  child: Text('Account not found.'),
                 ),
               );
-          }
+            }
 
-          return Padding(
-            padding: const EdgeInsets.all(8),
-            child: Center(
-              child: Text('Account not found.'),
-            ),
-          );
-        },
-      );
+            final tiles = [
+              _addressInfo(account.address.toString()),
+              _buildContent(
+                account: account.type.toString(),
+                label: "Type",
+              ),
+              _buildContent(
+                account: account.balance.toString(),
+                label: "Balance",
+              ),
+              _buildContent(
+                account: account.memorySize.toString(),
+                label: "Memory Size",
+              ),
+              _buildContent(
+                account: account.memoryAllowance.toString(),
+                label: "Memory Allowance",
+              ),
+            ];
+
+            final animated = tiles
+                .asMap()
+                .entries
+                .map(
+                  (e) => AnimationConfiguration.staggeredList(
+                    position: e.key,
+                    duration: const Duration(milliseconds: 375),
+                    child: SlideAnimation(
+                      verticalOffset: 50.0,
+                      child: FadeInAnimation(
+                        child: e.value,
+                      ),
+                    ),
+                  ),
+                )
+                .toList();
+
+            return Padding(
+              padding: const EdgeInsets.all(12),
+              child: AnimationLimiter(
+                child: ListView(
+                  children: animated,
+                ),
+              ),
+            );
+        }
+
+        return Padding(
+          padding: const EdgeInsets.all(8),
+          child: Center(
+            child: Text('Account not found.'),
+          ),
+        );
+      },
+    );
+  }
 }
