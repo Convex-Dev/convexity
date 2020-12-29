04ad2a33acbd6cad97e0fdd6b4b9ceebd3a0568f
Pedro Girardi <pedro@plando.work>
2020-12-29 16:12:58 +0700

    Address Book zero state

--- lib/screen/address_book.dart
+++ lib/screen/address_book.dart
@@ -1,6 +1,7 @@
 import 'package:convex_wallet/widget.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 import 'package:provider/provider.dart';
 
 import '../model.dart';
@@ -26,6 +27,29 @@ class AddressBookScreenBody extends StatelessWidget {
     final model = context.watch<AppState>().model;
 
     final contacts = model.contacts.toList(growable: false);
+
+    if (contacts.isEmpty) {
+      return Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              Icons.contacts,
+              size: 80,
+              color: Colors.black12,
+            ),
+            Gap(20),
+            Text(
+              'Your Address Book is empty.',
+              style: TextStyle(
+                color: Colors.black45,
+                fontSize: 16,
+              ),
+            ),
+          ],
+        ),
+      );
+    }
 
     return Padding(
       padding: const EdgeInsets.all(8),
