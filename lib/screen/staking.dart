98a82e8d57e18a97c418c3d58729454e37df983f
Pedro Girardi <pedro@plando.work>
2021-01-25 09:17:26 +0700

    Staking - query

--- lib/screen/staking.dart
+++ lib/screen/staking.dart
@@ -1,4 +1,7 @@
+import 'package:convex_wallet/convex.dart';
+import 'package:convex_wallet/model.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import '../widget.dart';
 
@@ -19,7 +22,31 @@ class StakingScreen extends StatelessWidget {
   }
 }
 
-class StakingScreenBody extends StatelessWidget {
+class StakingScreenBody extends StatefulWidget {
+  @override
+  _StakingScreenBodyState createState() => _StakingScreenBodyState();
+}
+
+class _StakingScreenBodyState extends State<StakingScreenBody> {
+  Future<Result> peers;
+
+  @override
+  void initState() {
+    super.initState();
+
+    final appState = context.read<AppState>();
+
+    final s = '''
+    (map 
+      (fn [[a m]]
+        [(str a) (:stake m) (:delegated-stake m)]) 
+      (:peers *state*))
+     ''';
+
+    peers = appState.convexClient().query(source: s);
+    peers.then((value) => print('Type ${value.value.runtimeType}'));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
