bc167e1e9bb946fcd82ec3837ab21a515715ef72
Pedro Girardi <pedro@plando.work>
2021-02-23 14:43:44 +0700

    Extract Widgets

--- lib/screen/exchange.dart
+++ lib/screen/exchange.dart
@@ -1,3 +1,4 @@
+import 'package:convex_wallet/convex.dart';
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
 
@@ -36,7 +37,10 @@ class _ExchangeScreenBodyState extends State<ExchangeScreenBody> {
   final buyIndex = 0;
   final sellIndex = 1;
 
-  var action;
+  ExchangeAction action;
+  Address ofToken;
+  int amount;
+  Address withToken;
 
   _ExchangeScreenBodyState({ExchangeAction action}) {
     this.action = action ?? ExchangeAction.buy;
@@ -48,82 +52,16 @@ class _ExchangeScreenBodyState extends State<ExchangeScreenBody> {
       child: SafeArea(
         child: Column(
           children: [
-            Center(
-              child: ToggleButtons(
-                children: [
-                  Text('Buy'),
-                  Text('Sell'),
-                ],
-                isSelected: [
-                  action == ExchangeAction.buy,
-                  action == ExchangeAction.sell,
-                ],
-                onPressed: (i) {
-                  setState(() {
-                    action = i == 0 ? ExchangeAction.buy : ExchangeAction.sell;
-                  });
-                },
-              ),
-            ),
+            Center(child: actionToggle()),
             Gap(40),
-            Row(
-              children: [
-                Text(action == ExchangeAction.buy ? 'Buy' : 'Sell'),
-                Gap(20),
-                ConstrainedBox(
-                  constraints: BoxConstraints.tightFor(width: 60, height: 60),
-                  child: ElevatedButton(
-                    child: Text(
-                      'CVX',
-                      style: Theme.of(context)
-                          .textTheme
-                          .caption
-                          .copyWith(color: Colors.white),
-                      overflow: TextOverflow.ellipsis,
-                    ),
-                    onPressed: () {},
-                    style: ElevatedButton.styleFrom(shape: CircleBorder()),
-                  ),
-                ),
-                Gap(30),
-                Text('Amount'),
-                Gap(10),
-                Expanded(
-                  child: TextField(
-                    onChanged: (s) {},
-                  ),
-                ),
-              ],
-            ),
+            buyOrSellAmount(),
             Gap(30),
-            Row(
-              children: [
-                Text('With'),
-                Gap(20),
-                ConstrainedBox(
-                  constraints: BoxConstraints.tightFor(width: 60, height: 60),
-                  child: ElevatedButton(
-                    child: Text(
-                      'CVX',
-                      style: Theme.of(context)
-                          .textTheme
-                          .caption
-                          .copyWith(color: Colors.white),
-                      overflow: TextOverflow.ellipsis,
-                    ),
-                    onPressed: () {},
-                    style: ElevatedButton.styleFrom(shape: CircleBorder()),
-                  ),
-                ),
-              ],
-            ),
+            buyOrSellWith(),
             Gap(50),
             SizedBox(
               width: double.infinity,
               child: ElevatedButton(
-                child: Text(
-                  action == ExchangeAction.buy ? 'Buy' : 'Sell',
-                ),
+                child: Text(actionText()),
                 onPressed: () {},
               ),
             )
@@ -132,4 +70,93 @@ class _ExchangeScreenBodyState extends State<ExchangeScreenBody> {
       ),
     );
   }
+
+  String actionText() {
+    switch (action) {
+      case ExchangeAction.buy:
+        return 'Buy';
+      case ExchangeAction.sell:
+        return 'Sell';
+    }
+
+    return '?';
+  }
+
+  Widget actionToggle() => ToggleButtons(
+        children: [
+          Text('Buy'),
+          Text('Sell'),
+        ],
+        isSelected: [
+          action == ExchangeAction.buy,
+          action == ExchangeAction.sell,
+        ],
+        onPressed: (i) {
+          setState(() {
+            action = i == 0 ? ExchangeAction.buy : ExchangeAction.sell;
+          });
+        },
+      );
+
+  Widget buyOrSellAmount() => Row(
+        children: [
+          Text(actionText()),
+          Gap(20),
+          ConstrainedBox(
+            constraints: BoxConstraints.tightFor(width: 60, height: 60),
+            child: ElevatedButton(
+              child: Text(
+                'CVX',
+                style: Theme.of(context)
+                    .textTheme
+                    .caption
+                    .copyWith(color: Colors.white),
+                overflow: TextOverflow.ellipsis,
+              ),
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                primary: Colors.orange,
+                shape: CircleBorder(),
+              ),
+            ),
+          ),
+          Gap(30),
+          Text('Amount'),
+          Gap(10),
+          Expanded(
+            child: TextField(
+              onChanged: (s) {
+                setState(() {
+                  amount = int.tryParse(s);
+                });
+              },
+            ),
+          ),
+        ],
+      );
+
+  Widget buyOrSellWith() => Row(
+        children: [
+          Text('With'),
+          Gap(20),
+          ConstrainedBox(
+            constraints: BoxConstraints.tightFor(width: 60, height: 60),
+            child: ElevatedButton(
+              child: Text(
+                'CVX',
+                style: Theme.of(context)
+                    .textTheme
+                    .caption
+                    .copyWith(color: Colors.white),
+                overflow: TextOverflow.ellipsis,
+              ),
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                primary: Colors.orange,
+                shape: CircleBorder(),
+              ),
+            ),
+          ),
+        ],
+      );
 }
