73425b1116e3606a8c63fe3147a86a1409839dd1
Pedro Girardi <pedro@plando.work>
2021-03-17 14:30:18 +0700

    ListView Top Currencies

--- lib/screen/top_tokens.dart
+++ lib/screen/top_tokens.dart
@@ -63,30 +63,39 @@ class _TopTokensScreenBodyState extends State<TopTokensScreenBody> {
             .where((e) => e.type == AssetType.fungible)
             .map((e) => e.asset as FungibleToken);
 
-        final fungibleRows = fungibles.map(
-          (token) => TableRow(
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+
+        return SafeArea(
+          child: ListView(
             children: [
-              TableCell(
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 8),
-                  child: Text(token.metadata.name),
-                ),
+              Dropdown<FungibleToken>(
+                active: _defaultToken ?? CVX,
+                items: [CVX, ...fungibles],
+                itemWidget: (FungibleToken token) {
+                  return Text(token.metadata.symbol);
+                },
+                onChanged: (t) {
+                  setState(() {
+                    _defaultToken = t == CVX ? null : t;
+
+                    _refreshPrices(
+                      context: context,
+                      assets: assets,
+                      withToken: _defaultToken?.address,
+                    );
+                  });
+                },
               ),
-              TableCell(
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 8),
-                  child: Text(
-                    token.metadata.symbol,
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-              ),
-              TableCell(
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 8),
-                  child: FutureBuilder<Result>(
+              ...fungibles.map(
+                (token) => ListTile(
+                  leading: Icon(Icons.attach_money),
+                  title: Text(token.metadata.symbol),
+                  subtitle: Text(token.metadata.name),
+                  trailing: FutureBuilder<Result>(
                     future: _prices,
                     builder: (context, snapshot) {
                       final data = snapshot.data?.value ?? [];
@@ -123,55 +132,6 @@ class _TopTokensScreenBodyState extends State<TopTokensScreenBody> {
               ),
             ],
           ),
-        );
-
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return Center(
-            child: CircularProgressIndicator(),
-          );
-        }
-
-        return Table(
-          children: [
-            TableRow(
-              children: [
-                TableCell(
-                  child: columnText('Name'),
-                ),
-                TableCell(
-                  child: columnText('Symbol'),
-                ),
-                TableCell(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      columnText('Price'),
-                      Gap(10),
-                      Dropdown<FungibleToken>(
-                        active: _defaultToken ?? CVX,
-                        items: [CVX, ...fungibles],
-                        itemWidget: (FungibleToken token) {
-                          return Text(token.metadata.symbol);
-                        },
-                        onChanged: (t) {
-                          setState(() {
-                            _defaultToken = t == CVX ? null : t;
-
-                            _refreshPrices(
-                              context: context,
-                              assets: assets,
-                              withToken: _defaultToken?.address,
-                            );
-                          });
-                        },
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-            ...fungibleRows,
-          ],
         );
       },
     );
