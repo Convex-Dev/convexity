fad6d0c666eb0be1f4b7171fe74b3a6612845725
Pedro Girardi <pedro@plando.work>
2021-04-13 18:55:29 +0700

    Extract NonFungible GridTile

--- lib/screen/non_fungible_market.dart
+++ lib/screen/non_fungible_market.dart
@@ -1,11 +1,54 @@
+import 'package:convex_wallet/widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
+import 'package:provider/provider.dart';
+
+import '../model.dart';
 
 class NonFungibleMarketScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final appState = context.watch<AppState>();
+
+    final convexClient = appState.convexClient();
+
     return Scaffold(
       appBar: AppBar(title: Text('Non-Fungible Market')),
-      body: Container(),
+      body: Container(
+        padding: defaultScreenPadding,
+        child: AnimationLimiter(
+          child: GridView.count(
+            crossAxisSpacing: 6,
+            mainAxisSpacing: 6,
+            crossAxisCount: 2,
+            // TODO Token ID
+            children: [0].asMap().entries.map(
+              (entry) {
+                // TODO Actor address
+                final dataSource =
+                    '(call #702 (get-token-data ${entry.value}))';
+
+                final data = convexClient.query(source: dataSource);
+
+                return AnimationConfiguration.staggeredGrid(
+                  position: entry.key,
+                  duration: const Duration(milliseconds: 375),
+                  columnCount: 2,
+                  child: ScaleAnimation(
+                    child: FadeInAnimation(
+                      child: NonFungibleGridTile(
+                        tokenId: entry.value,
+                        data: data,
+                        onTap: () {},
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ).toList(),
+          ),
+        ),
+      ),
     );
   }
 }
