4566fa9f2d73fdc448a51b0356826cfa61f76231
Pedro Girardi <pedro@plando.work>
2021-06-18 17:14:28 +0700

    Layout

--- lib/screen/social_currency.dart
+++ lib/screen/social_currency.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import '../widget.dart';
 import '../model.dart';
 import '../nav.dart' as nav;
 
@@ -11,22 +12,27 @@ class SocialCurrencyScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final appState = context.watch<AppState>();
 
-    print(appState.model.socialCurrency);
-
     return Scaffold(
       appBar: AppBar(
         title: Text('Social Currency'),
       ),
-      body: appState.model.socialCurrency != null
-          ? Text(appState.model.socialCurrency.toString())
-          : Center(
-              child: ElevatedButton(
-                child: const Text('Create Social Currency'),
-                onPressed: () {
-                  nav.pushNewSocialCurrency(context);
-                },
+      body: Container(
+        padding: defaultScreenPadding,
+        child: appState.model.socialCurrency != null
+            ? Column(
+                children: [
+                  Center(child: Text(appState.model.socialCurrency.toString())),
+                ],
+              )
+            : Center(
+                child: ElevatedButton(
+                  child: const Text('Create Social Currency'),
+                  onPressed: () {
+                    nav.pushNewSocialCurrency(context);
+                  },
+                ),
               ),
-            ),
+      ),
     );
   }
 }
